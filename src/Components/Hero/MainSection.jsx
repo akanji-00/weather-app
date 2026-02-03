7de62b2f544da14feaed42b5e69742513b1ebd68
Mohammed Adetunji <theadetunji001@gmail.com>
2026-02-04 00:21:25 +0100

    hero section started and styling currently

--- src/Components/Hero/MainSection.jsx
+++ src/Components/Hero/MainSection.jsx
@@ -1,7 +1,18 @@
+import WeatherIcon from "../../assets/images/icon-sunny.webp";
+
 export default function MainSection() {
   return (
     <section className="hero__section">
-      <div className="weather__box">Weather Box</div>
+      <div className="weather__box">
+        <div className="weather__box--text">
+          <p className="text-country">Berlin, Germany</p>
+          <p className="text-date">Tuesday, Aug 5, 2025</p>
+        </div>
+        <div className="weather__box--detail">
+          <img src={WeatherIcon} />
+          <p className="text-temp">20</p>
+        </div>
+      </div>
       <div className="weather__tiles">Weather Tiles</div>
       <div className="weather__daily--forecast">Weather Daily Forecast</div>
       <div className="weather__hourly--forecast">Weather Hourly Forecast</div>
