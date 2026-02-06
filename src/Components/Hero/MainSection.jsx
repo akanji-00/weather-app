6e7231bdaefe704a9329db069772716a65bc7ea4
Mohammed Adetunji <theadetunji001@gmail.com>
2026-02-06 12:16:11 +0100

    Tweaks

--- src/Components/Hero/MainSection.jsx
+++ src/Components/Hero/MainSection.jsx
@@ -2,20 +2,27 @@ import WeatherIcon from "../../assets/images/icon-sunny.webp";
 
 export default function MainSection() {
   return (
-    <section className="hero__section">
-      <div className="weather__box">
-        <div className="weather__box--text">
-          <p className="text-country">Berlin, Germany</p>
-          <p className="text-date">Tuesday, Aug 5, 2025</p>
+    <main className="main__section">
+      <section className="hero__section">
+        <div className="weather__box">
+          <div className="weather__box--text">
+            <p className="text-country">Berlin, Germany</p>
+            <p className="text-date">Tuesday, Aug 5, 2025</p>
+          </div>
+          <div className="weather__box--detail">
+            <img src={WeatherIcon} />
+            <p className="text-temp">20</p>
+          </div>
         </div>
-        <div className="weather__box--detail">
-          <img src={WeatherIcon} />
-          <p className="text-temp">20</p>
-        </div>
-      </div>
-      <div className="weather__tiles">Weather Tiles</div>
-      <div className="weather__daily--forecast">Weather Daily Forecast</div>
-      <div className="weather__hourly--forecast">Weather Hourly Forecast</div>
-    </section>
+      </section>
+
+      <section className="weather__tiles">Weather Tiles</section>
+      <section className="weather__daily--forecast">
+        Weather Daily Forecast
+      </section>
+      <section className="weather__hourly--forecast">
+        Weather Hourly Forecast
+      </section>
+    </main>
   );
 }
